ffa9185742492b4135df1b5731ce60d765e95416
mog-labs <zackdykes@gmail.com>
2010-03-11 18:07:32 +0100

    fixed lightbox for IE (6,7,8). no overlay in IE6, IE7.

--- ContentFlowAddOn_lightbox.js
+++ ContentFlowAddOn_lightbox.js
@@ -21,44 +21,12 @@ new ContentFlowAddOn ('lightbox', {
     },
 	
 	ContentFlowConf: {
-        loadingTimeout: 60000,          // milliseconds
-        activeElement: 'content',       // item or content
-
-        maxItemHeight: 0,               // 0 == auto, >0 max item height in px
-        scaleFactor: 1.0,               // overall scale factor of content
-        scaleFactorLandscape: 1.33,     // scale factor of landscape images ('max' := height= maxItemHeight)
-        scaleFactorPortrait: 1.0,       // scale factor of portraoit and square images ('max' := width = item width)
-        fixItemSize: false,             // don't scale item size to fit image, crop image if bigger than item
-        relativeItemPosition: "top center", // align top/above, bottom/below, left, right, center of position coordinate
-
-        circularFlow: true,             // should the flow wrap around at begging and end?
-        verticalFlow: false,            // turn ContentFlow 90 degree counterclockwise
-        visibleItems: -1,               // how man item are visible on each side (-1 := auto)
-        endOpacity: 1,                  // opacity of last visible item on both sides
-        startItem:  "center",           // which item should be shown on startup?
-        scrollInFrom: "pre",            // from where should be scrolled in?
-
-        flowSpeedFactor: 1.0,           // how fast should it scroll?
-        flowDragFriction: 1.0,          // how hard should it be be drag the floe (0 := no dragging)
-        scrollWheelSpeed: 1.0,          // how fast should the mouse wheel scroll. nagive values will revers the scroll direction (0:= deactivate mouse wheel)
-        keys: {                         // key => function definition, if set to {} keys ar deactivated
-            13: function () { this.conf.onclickActiveItem(this._activeItem) },
-            37: function () { this.moveTo('pre') },
-            38: function () { this.moveTo('visibleNext') },
-            39: function () { this.moveTo('next') },
-            40: function () { this.moveTo('visiblePre') }
-        },
-
-        reflectionColor: "transparent", // none, transparent, overlay or hex RGB CSS style #RRGGBB
-        reflectionHeight: 0.5,          // float (relative to original image height)
-        reflectionGap: 0.0,             // gap between the image and the reflection
-
         onclickInactiveItem: function (item) {
             this.conf.onclickActiveItem(item);
         },
 
         onclickActiveItem: function (item) {            
-            var url, target, content;
+            var url, target;
             if (item.element['href']){
                 url = item.element['href'];
                 return false; // prevent default in IE
@@ -70,7 +38,7 @@ new ContentFlowAddOn ('lightbox', {
             if (typeof url !== "undefined" && url !== null) {
                 if (item.element.getAttribute('target')) {
                     target = item.element.getAttribute('target')
-                    if (typeof target !== "undefined" && target !== null) {
+                    if (typeof url !== "undefined" && url !== null) {
                         window.open(url, target).focus();
                     }
                 }
@@ -79,27 +47,18 @@ new ContentFlowAddOn ('lightbox', {
                 }
             }
             else {
-                content = item.content;
-                if (item.content['src']) {
-                    if (! item.element['href']) {
-                    //if (! item.element.getAttribute('href') && ! item.content.getAttribute('href')) {
-                        item.content.href = content['src'];
+                var content = item.content;
+                if (content.getAttribute('src')) {
+                    if (! item.element.getAttribute('href') && ! item.content.getAttribute('href')) {
+                        //
+                        item.content.setAttribute ('href', content.getAttribute('src')); // IE8
+                        item.content.href = content.getAttribute('src'); // other browsers!
                     }
                     if (item.caption) {
                         item.content.setAttribute ('title', item.caption.innerHTML);
                     }
+                    showLightbox(item.content);
                 }
-                else if (content.getAttribute('src')) {
-                    if (! item.element.getAttribute('href')) {
-                    //if (! item.element.getAttribute('href') && ! item.content.getAttribute('href')) {
-                        item.content.href = content.getAttribute('src');
-                    }
-                    if (item.caption) {
-                        item.content.setAttribute ('title', item.caption.innerHTML);
-                    }
-                    //showLightbox(item.content);
-                }
-                showLightbox(item.content);
             }
         }
     }
